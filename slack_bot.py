a2eb3be1fcde2f8bc42b86fd1ac916314accdf1e
iraynovskyy <64488755+iraynovskyy@users.noreply.github.com>
2023-04-21 06:11:44 +0300

    Add files

---
+++ slack_bot.py
@@ -0,0 +1,32 @@
+import os
+import time
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+
+SLACK_API_TOKEN = 'xoxb-4348796765079-5117507520707-JtVr88EVAmlzVCxTZvpeqLDy'
+
+# Set the Slack API token
+# client = WebClient(token=os.environ['SLACK_API_TOKEN'])
+client = WebClient(token=SLACK_API_TOKEN)
+
+# Set the channel ID or name
+channel_id = 'slack-bot'
+
+# Set the message text
+user_id = 'U04AGMSV29L'
+message_text = f'Hello, world test Ihor <@{user_id}>!'
+# message_text = 'Hello, world test Ihor ed !'
+
+while True:
+    try:
+        # Call the chat.postMessage API method with the channel ID and message text
+        response = client.chat_postMessage(
+            channel=channel_id,
+            text=message_text
+        )
+        print('Message sent: ', response['ts'])
+    except SlackApiError as e:
+        print('Error sending message: ', e)
+
+    # Wait for 1 minute
+    time.sleep(5)
