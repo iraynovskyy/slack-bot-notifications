cdbb1a6c6747fdcdc248055510f7e7628507820b
iranovskyy <iraynovskyy@spscommerce.com>
2023-04-24 04:36:57 +0300

    update to run every day to post at 9:25 a motivational video into slack channel

--- slack_bot.py
+++ slack_bot.py
@@ -2,23 +2,16 @@ import os
 import time
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
+import schedule
 
 # SLACK_API_TOKEN = '<is in .env locally>'
 
-# from cachetools import cached, TTLCache
-# import boto3
-#
-#
-# @cached(TTLCache(maxsize=128, ttl=300))
-# def get_token(name):
-#     _client = boto3.client("ssm", region_name='us-east-1')
-#     return _client.get_parameter(Name=name, WithDecryption=True)['Parameter']['Value']
 import boto3
 
 ssm = boto3.client('ssm', region_name='eu-north-1')
 response = ssm.get_parameter(Name='ihor-slack-bot-notification-token', WithDecryption=True)
 SLACK_API_TOKEN = response['Parameter']['Value']
-print('SLACK_API_TOKEN===', SLACK_API_TOKEN)
+# print('SLACK_API_TOKEN===', SLACK_API_TOKEN)
 
 # Set the Slack API token
 # client = WebClient(token=os.environ['SLACK_API_TOKEN'])
@@ -29,10 +22,25 @@ channel_id = 'slack-bot'
 
 # Set the message text
 user_id = 'U04AGMSV29L'
-message_text = f'Hello, world test Ihor <@{user_id}>!'
+message_text = f'do not lose your life... https://www.youtube.com/shorts/Oy9kDTuHjKQ, https://www.youtube.com/watch?v=u_ktRTWMX3M&ab_channel=MulliganBrothers, <@{user_id}>'
 # message_text = 'Hello, world test Ihor ed !'
 
-while True:
+# while True:
+#     try:
+#         # Call the chat.postMessage API method with the channel ID and message text
+#         response = client.chat_postMessage(
+#             channel=channel_id,
+#             text=message_text
+#         )
+#         print('Message sent: ', response['ts'])
+#     except SlackApiError as e:
+#         print('Error sending message: ', e)
+#
+#     # Wait for 1 minute
+#     time.sleep(5)
+
+print('app started! working...')
+def send_message():
     try:
         # Call the chat.postMessage API method with the channel ID and message text
         response = client.chat_postMessage(
@@ -43,5 +51,10 @@ while True:
     except SlackApiError as e:
         print('Error sending message: ', e)
 
-    # Wait for 1 minute
-    time.sleep(5)
+# Schedule the send_message function to run every day at 9am
+schedule.every().day.at("09:25").do(send_message)
+
+# Loop forever, checking if the scheduled function should be run
+while True:
+    schedule.run_pending()
+    time.sleep(50)
