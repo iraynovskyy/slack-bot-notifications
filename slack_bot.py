9fbfbad626f7fe7e0b65a7ec8f7915dc0ac45487
iranovskyy <iraynovskyy@spscommerce.com>
2023-04-24 10:38:15 +0300

    update schedule notes

--- slack_bot.py
+++ slack_bot.py
@@ -40,6 +40,17 @@ message_text = f'do not lose your life... https://www.youtube.com/shorts/Oy9kDTu
 #     time.sleep(5)
 
 print('app started! working...')
+
+
+# Define a function to print 'sport'
+def print_do_sport():
+    print(f'do sport 15 mins! <@{user_id}>, for own better health, look and social life')
+
+
+def print_do_courses():
+    print(f'watch courses 20-30 mins! <@{user_id}>, for self-development and pass the table of Progress...BASICS...architecture')
+
+
 def send_message():
     try:
         # Call the chat.postMessage API method with the channel ID and message text
@@ -51,8 +62,15 @@ def send_message():
     except SlackApiError as e:
         print('Error sending message: ', e)
 
+
 # Schedule the send_message function to run every day at 9am
 schedule.every().day.at("09:25").do(send_message)
+
+# Schedule the print_do_sport function to run every day at 10am
+schedule.every().day.at("9:55").do(print_do_sport())
+
+# Schedule the print_do_cources function to run every day at 5:30pm
+schedule.every().day.at("17:30").do(print_do_courses())
 
 # Loop forever, checking if the scheduled function should be run
 while True:
