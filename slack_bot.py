8c24c0e9e76a6ba5f8e72c634e61e49904307cda
iranovskyy <iraynovskyy@spscommerce.com>
2023-04-27 19:00:20 +0300

    update notifications to be sent into slack daily with 3 schedules on diff timing

--- slack_bot.py
+++ slack_bot.py
@@ -23,57 +23,45 @@ channel_id = 'slack-bot'
 
 # Set the message text
 user_id = 'U04AGMSV29L'
-message_text = f'do not lose your life... https://www.youtube.com/shorts/Oy9kDTuHjKQ, https://www.youtube.com/watch?v=u_ktRTWMX3M&ab_channel=MulliganBrothers, <@{user_id}>'
-# message_text = 'Hello, world test Ihor ed !'
-
-# while True:
-#     try:
-#         # Call the chat.postMessage API method with the channel ID and message text
-#         response = client.chat_postMessage(
-#             channel=channel_id,
-#             text=message_text
-#         )
-#         print('Message sent: ', response['ts'])
-#     except SlackApiError as e:
-#         print('Error sending message: ', e)
-#
-#     # Wait for 1 minute
-#     time.sleep(5)
+message_motivation = f'do not lose your life... https://www.youtube.com/shorts/Oy9kDTuHjKQ, https://www.youtube.com/watch?v=u_ktRTWMX3M&ab_channel=MulliganBrothers, <@{user_id}>'
+message_sport = f'do sport 15 mins! <@{user_id}>, for own better health, look and social life'
+message_courses = f'watch courses 20-30 mins! <@{user_id}>, for self-development and pass the table of Progress...BASICS...architecture'
 
 print('app started! working...')
 
 
-# Define a function to print 'sport'
-def print_do_sport():
-    print(f'do sport 15 mins! <@{user_id}>, for own better health, look and social life')
-
-
-def print_do_courses():
-    print(f'watch courses 20-30 mins! <@{user_id}>, for self-development and pass the table of Progress...BASICS...architecture')
-
-
-def send_message():
+def send_message(msg):
     try:
         # Call the chat.postMessage API method with the channel ID and message text
         response = client.chat_postMessage(
             channel=channel_id,
-            text=message_text
+            text=msg
         )
         print('Message sent: ', response['ts'])
     except SlackApiError as e:
         print('Error sending message: ', e)
 
 
-# Schedule the send_message function to run every day at 9am
-schedule.every().day.at("06:25").do(send_message)
+# send starting message
+send_message('The app has started working! Have a Great Day!')
 
-# Schedule the print_do_sport function to run every day at 10am
-schedule.every().day.at("06:55").do(print_do_sport)
+# # Schedule the send_message function to Motivate every day at 9:am
+schedule.every().day.at("18:56", "Europe/Kyiv").do(
+    lambda: send_message(message_motivation))  # todo add logic to motivate again at 23:00
 
-# Schedule the print_do_cources function to run every day at 5:30pm
-schedule.every().day.at("14:30").do(print_do_courses)
+# Schedule the print_do_sport function to Motivate every day at 11pm
+schedule.every().day.at("23:00", "Europe/Kyiv").do(
+    lambda: send_message(message_motivation))  # todo add some logic to press OK not OK...gather statisctics?!
+
+# # Schedule the print_do_sport function for Sport every day at 10am
+schedule.every().day.at("09:45", "Europe/Kyiv").do(
+    lambda: send_message(message_sport))
+
+# # Schedule the print_do_cources function for Self-Development run every day at 5:30pm
+schedule.every().day.at("14:30", "Europe/Kyiv").do(
+    lambda: send_message(message_courses))
 
 # Loop forever, checking if the scheduled function should be run
 while True:
     schedule.run_pending()
-    time.sleep(50)
+    time.sleep(1)
